68a4d78b626a8faa3b5303f27f6876cbf77982ea
ApotoxinElish <19079181d@connect.polyu.hk>
2021-09-27 18:09:18 +0800

    Update

--- Assets/Scripts/CardDataBase.cs
+++ Assets/Scripts/CardDataBase.cs
@@ -8,10 +8,10 @@ public class CardDataBase : MonoBehaviour
 
     void Awake()
     {
-        cardList.Add(new Card(0, "None", 0, 0, "None", Resources.Load<Sprite>("1")));
+        cardList.Add(new Card(0, "None", 0, 0, "None", Resources.Load<Sprite>("0")));
         cardList.Add(new Card(1, "Elf", 2, 1000, "It's Elf", Resources.Load<Sprite>("1")));
-        cardList.Add(new Card(2, "Dwarf", 3, 3000, "It's Dwarf", Resources.Load<Sprite>("1")));
-        cardList.Add(new Card(3, "Human", 5, 6000, "It's Human", Resources.Load<Sprite>("1")));
-        cardList.Add(new Card(4, "Demon", 1, 1000, "It's Demon", Resources.Load<Sprite>("1")));
+        cardList.Add(new Card(2, "Dwarf", 3, 3000, "It's Dwarf", Resources.Load<Sprite>("2")));
+        cardList.Add(new Card(3, "Human", 5, 6000, "It's Human", Resources.Load<Sprite>("3")));
+        cardList.Add(new Card(4, "Demon", 1, 1000, "It's Demon", Resources.Load<Sprite>("4")));
     }
 }
