3b0df03cf451c5c8da996125393d9bb69754542c
ApotoxinElish <19079181d@connect.polyu.hk>
2022-05-25 20:39:58 +0800

    Update

--- Assets/Scripts/Systems/FirstGameSetup.cs
+++ Assets/Scripts/Systems/FirstGameSetup.cs
@@ -2,51 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FirstGameSetup : MonoBehaviour
+namespace AHLCG
 {
-    private void Initialize()
+    public class FirstGameSetup : MonoBehaviour
     {
-        ChooseInvestigators();
-        GatherDecks();
-        ChooseLeadInvestigator();
-        AssembleTokenPool();
-        AssembletheChaosBag();
-        TakeStartingResources();
-        DrawOpeningHand();
-    }
+        private void Initialize()
+        {
+            ChooseInvestigators();
+            GatherDecks();
+            ChooseLeadInvestigator();
+            AssembleTokenPool();
+            AssembletheChaosBag();
+            TakeStartingResources();
+            DrawOpeningHand();
+        }
 
-    private void ChooseInvestigators()
-    {
+        private void ChooseInvestigators()
+        {
 
-    }
+        }
 
-    private void GatherDecks()
-    {
+        private void GatherDecks()
+        {
 
-    }
+        }
 
-    private void ChooseLeadInvestigator()
-    {
+        private void ChooseLeadInvestigator()
+        {
 
-    }
+        }
 
-    private void AssembleTokenPool()
-    {
+        private void AssembleTokenPool()
+        {
 
-    }
+        }
 
-    private void AssembletheChaosBag()
-    {
+        private void AssembletheChaosBag()
+        {
 
-    }
+        }
 
-    private void TakeStartingResources()
-    {
+        private void TakeStartingResources()
+        {
 
-    }
+        }
 
-    private void DrawOpeningHand()
-    {
+        private void DrawOpeningHand()
+        {
 
+        }
     }
 }
--- Assets/Scripts/Systems/ScenarioSetup.cs
+++ Assets/Scripts/Systems/ScenarioSetup.cs
@@ -2,33 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScenarioSetup : MonoBehaviour
+namespace AHLCG
 {
-    private void Initialize()
+    public class ScenarioSetup : MonoBehaviour
     {
-        GatherScenarioCards();
-        SetAgendaDeck();
-        SetActDeck();
-        PlaceReferenceCard();
-        PlaceLocations();
-        AssembleEncounterDeck();
-        ReadScenarioIntroduction();
-        PlaceInvestigators();
+        private void Initialize()
+        {
+            GatherScenarioCards();
+            SetAgendaDeck();
+            SetActDeck();
+            PlaceReferenceCard();
+            PlaceLocations();
+            AssembleEncounterDeck();
+            ReadScenarioIntroduction();
+            PlaceInvestigators();
+        }
+
+        private void GatherScenarioCards() { }
+
+        private void SetAgendaDeck() { }
+
+        private void SetActDeck() { }
+
+        private void PlaceReferenceCard() { }
+
+        private void PlaceLocations() { }
+
+        private void AssembleEncounterDeck() { }
+
+        private void ReadScenarioIntroduction() { }
+
+        private void PlaceInvestigators() { }
     }
-
-    private void GatherScenarioCards() { }
-
-    private void SetAgendaDeck() { }
-
-    private void SetActDeck() { }
-
-    private void PlaceReferenceCard() { }
-
-    private void PlaceLocations() { }
-
-    private void AssembleEncounterDeck() { }
-
-    private void ReadScenarioIntroduction() { }
-
-    private void PlaceInvestigators() { }
 }
